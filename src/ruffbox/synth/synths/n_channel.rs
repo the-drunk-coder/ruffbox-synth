fb16fe8fe0b2810162149fd227a512f8fa134cec
Niklas Reppel <nik@parkellipsen.de>
2022-04-10 15:43:12 +0200

    add a wavetable synth

--- src/ruffbox/synth/synths/n_channel.rs
+++ src/ruffbox/synth/synths/n_channel.rs
@@ -1,11 +1,13 @@
+// a collection of pre-fabricated synths
+
 pub mod lf_cub_synth;
 pub mod lf_saw_synth;
 pub mod lf_square_synth;
 pub mod lf_tri_synth;
 pub mod n_channel_sampler;
 pub mod risset_bell;
-/// a collection of pre-fabricated synths
 pub mod sine_synth;
+pub mod wavetable_synth;
 
 pub use crate::ruffbox::synth::synths::n_channel::lf_cub_synth::LFCubSynth;
 pub use crate::ruffbox::synth::synths::n_channel::lf_saw_synth::LFSawSynth;
@@ -14,3 +16,4 @@ pub use crate::ruffbox::synth::synths::n_channel::lf_tri_synth::LFTriSynth;
 pub use crate::ruffbox::synth::synths::n_channel::n_channel_sampler::NChannelSampler;
 pub use crate::ruffbox::synth::synths::n_channel::risset_bell::RissetBell;
 pub use crate::ruffbox::synth::synths::n_channel::sine_synth::SineSynth;
+pub use crate::ruffbox::synth::synths::n_channel::wavetable_synth::WavetableSynth;
