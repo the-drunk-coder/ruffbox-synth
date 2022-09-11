322634319b67c990544e0c0191dc7f5e14f40f8a
Niklas Reppel <nik@parkellipsen.de>
2022-09-11 19:38:39 -0400

    compacting the code

--- src/synths.rs
+++ src/synths.rs
@@ -2,20 +2,9 @@ pub mod ambisonic;
 pub mod n_channel;
 
 // channel-based synths
-pub use crate::synths::n_channel::fm_saw_synth::FMSawSynth;
-pub use crate::synths::n_channel::fm_square_synth::FMSquareSynth;
-pub use crate::synths::n_channel::fm_tri_synth::FMTriSynth;
-pub use crate::synths::n_channel::lf_cub_synth::LFCubSynth;
-pub use crate::synths::n_channel::lf_saw_synth::LFSawSynth;
-pub use crate::synths::n_channel::lf_square_synth::LFSquareSynth;
-pub use crate::synths::n_channel::lf_tri_synth::LFTriSynth;
 pub use crate::synths::n_channel::n_channel_sampler::NChannelSampler;
 pub use crate::synths::n_channel::risset_bell::RissetBell;
-pub use crate::synths::n_channel::sine_synth::SineSynth;
 pub use crate::synths::n_channel::single_oscillator_synth::SingleOscillatorSynth;
-pub use crate::synths::n_channel::wavematrix_synth::WavematrixSynth;
-pub use crate::synths::n_channel::wavetable_synth::WavetableSynth;
-pub use crate::synths::n_channel::wt_saw_synth::WTSawSynth;
 
 // ambisonic synths
 pub use crate::synths::ambisonic::ambisonic_sampler_o1::AmbisonicSamplerO1;
--- src/synths/n_channel.rs
+++ src/synths/n_channel.rs
@@ -1,36 +1,8 @@
 // a collection of pre-fabricated synths
-
-pub mod lf_cub_synth;
-pub mod lf_saw_synth;
-pub mod lf_square_synth;
-pub mod lf_tri_synth;
 pub mod n_channel_sampler;
 pub mod risset_bell;
-pub mod sine_synth;
 pub mod single_oscillator_synth;
-pub mod wavematrix_synth;
-pub mod wavetable_synth;
 
-pub mod fm_saw_synth;
-pub mod fm_square_synth;
-pub mod fm_tri_synth;
-pub mod wt_saw_synth;
-
-pub use crate::synths::n_channel::single_oscillator_synth::SingleOscillatorSynth;
-
-pub use crate::synths::n_channel::lf_cub_synth::LFCubSynth;
-pub use crate::synths::n_channel::lf_saw_synth::LFSawSynth;
-
-pub use crate::synths::n_channel::lf_square_synth::LFSquareSynth;
-pub use crate::synths::n_channel::lf_tri_synth::LFTriSynth;
 pub use crate::synths::n_channel::n_channel_sampler::NChannelSampler;
 pub use crate::synths::n_channel::risset_bell::RissetBell;
-pub use crate::synths::n_channel::sine_synth::SineSynth;
-pub use crate::synths::n_channel::wavematrix_synth::WavematrixSynth;
-pub use crate::synths::n_channel::wavetable_synth::WavetableSynth;
-
-pub use crate::synths::n_channel::wt_saw_synth::WTSawSynth;
-
-pub use crate::synths::n_channel::fm_saw_synth::FMSawSynth;
-pub use crate::synths::n_channel::fm_square_synth::FMSquareSynth;
-pub use crate::synths::n_channel::fm_tri_synth::FMTriSynth;
+pub use crate::synths::n_channel::single_oscillator_synth::SingleOscillatorSynth;
