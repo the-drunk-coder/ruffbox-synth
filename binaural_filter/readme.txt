5d1488c19b4b8a325bc0e80097c1475f232087a3
Niklas Reppel <nik@parkellipsen.de>
2023-02-01 00:26:58 +0100

    add default filter

---
+++ binaural_filter/readme.txt
@@ -0,0 +1,1 @@
+Defualt HRIR is based on: http://audiogroup.web.th-koeln.de/ku100hrir.html
--- src/building_blocks/ambisonics/binauralizer_o1.rs
+++ src/building_blocks/ambisonics/binauralizer_o1.rs
@@ -1,5 +1,7 @@
 use crate::building_blocks::convolver::block_convolver::BlockConvolver;
 
+const DEFAULT_FILTER : &'static [u8] = include_bytes!("../../../binaural_filter/default.raw");
+
 /**
  * a simple first-order convolution binauralizer
  */
@@ -9,6 +11,25 @@ pub struct BinauralizerO1<const BUFSIZE: usize> {
 }
 
 impl<const BUFSIZE: usize> BinauralizerO1<BUFSIZE> {
+
+    pub fn default_filter() -> Self {
+	 let ir: Vec<f32> = DEFAULT_FILTER
+            .chunks(4)
+            .map(|b| (f32::from_le_bytes(b.try_into().unwrap()) as f32))
+            .collect();
+
+	debug_assert!(ir.len() == 1024);
+	
+	let ir_proc :Vec<(Vec<f32>, Vec<f32>)> = vec![
+	    (ir[0..128].to_vec(), ir[512..640].to_vec()),
+	    (ir[128..256].to_vec(), ir[640..768].to_vec()),
+	    (ir[256..384].to_vec(), ir[768..896].to_vec()),
+	    (ir[384..512].to_vec(), ir[896..1024].to_vec()),
+	];
+	
+	BinauralizerO1::from_ir(ir_proc)
+    }
+
     // initialize with unit IRs
     pub fn from_ir(ir: Vec<(Vec<f32>, Vec<f32>)>) -> Self {
         let mut left = Vec::new();
@@ -35,5 +56,15 @@ impl<const BUFSIZE: usize> BinauralizerO1<BUFSIZE> {
         }
 
         bin_block
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    #[test]
+    fn test_load() {
+	let bin = BinauralizerO1::<128>::default_filter();
     }
 }
